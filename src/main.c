4cc3d81a975588bf590ca431c95ee004024c7cf6
tarxton <89666132+tarxton@users.noreply.github.com>
2025-06-02 19:21:32 +0200

    added gates and logic, other geometry and connections, text, and controls

--- src/main.c
+++ src/main.c
@@ -6,15 +6,19 @@
 #define HEIGHT 600
 #define RECT_DIM 100.0f /* dimensions of input blocks */
 
+/* initialize window and renderer variables */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
+/* ini background variables */
 static SDL_Surface *bg_surface = NULL;
 static SDL_Texture *bg_texture = NULL;
 
+/* booleans for activated gates */
 static bool active[4] = {false, false, false, false};
 static bool gate_active[3] = {false, false, false};
 
+/* all geometry and drawings (rendering) */
 static SDL_FRect rects[4];
 static SDL_FRect borders[4];
 static SDL_FRect result, result_border;
@@ -23,10 +27,11 @@ static SDL_FRect gates[3];
 static SDL_FRect gate_borders[3];
 static SDL_FRect connect_wire, left_wire, right_wire;
 
+/* operators and their gates */
 static char operations[5][5] = {"AND", "OR", "NOR", "nAND", "XOR"};
 static char gate_op[3][5];
 
-// and or not nor xor, no not because its not binary
+// and or nand nor xor, no not because its not binary
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) /* runs once at startup, initialization */
 {
@@ -69,7 +74,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) /* runs once 
     gates[0].x = wires[0].x + wires[0].w;
     gates[0].y = rects[0].y;
     gates[0].w = 150.0f;
-    gates[0].h = 320.0f; //{, rects[0].y, 150.0f, 300.0f};
+    gates[0].h = 320.0f; 
 
     gate_borders[0].x = gates[0].x - 10.0f;
     gate_borders[0].y = gates[0].y - 10.0f;
@@ -79,7 +84,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) /* runs once 
     gates[1].x = wires[0].x + wires[0].w;
     gates[1].y = rects[2].y;
     gates[1].w = 150.0f;
-    gates[1].h = 320.0f; //{wires[0].x + wires[0].w, rects[2].y, 150.0f, 300.0f};
+    gates[1].h = 320.0f; 
 
     gate_borders[1].x = gates[1].x - 10.0f;
     gate_borders[1].y = gates[1].y - 10.0f;
@@ -113,7 +118,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) /* runs once 
 
     for (int i = 0; i < SDL_arraysize(gate_op); i++)
     {
-        strcpy(gate_op[i], operations[SDL_rand(5)]);
+        strcpy(gate_op[i], operations[SDL_rand(5)]); /* randomly assigned gates */
     }
 
     return SDL_APP_CONTINUE; /* app continues if window created */
@@ -121,24 +126,24 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) /* runs once 
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    if (event->type == SDL_EVENT_KEY_DOWN && event->key.key == SDLK_Q|| event->type == SDL_EVENT_QUIT) /* quit on key down */
+    if (event->type == SDL_EVENT_KEY_DOWN && event->key.key == SDLK_Q|| event->type == SDL_EVENT_QUIT) /* press 'q' to quit */
     {
         return SDL_APP_SUCCESS; /* end program reporting success */
     }
 
-    if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN)
+    if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN)  /* click to activate input gates */
     {
         for (int i = 0; i < SDL_arraysize(rects); i++)
         {
             if (event->button.x >= rects[i].x && event->button.x <= (rects[i].x + rects[i].w) &&
-                event->button.y >= rects[i].y && event->button.y <= (rects[i].y + rects[i].h))
+                event->button.y >= rects[i].y && event->button.y <= (rects[i].y + rects[i].h)) /* checking if clicked on gate rect */
             {
-                active[i] = !active[i];
+                active[i] = !active[i]; /* turning input gates on or off */
             }
         }
     }
 
-    if (event->key.key == SDLK_R)
+    if (event->key.key == SDLK_R) /* press 'r' to reinitialize all values, effectively restarting */
     {
         for (int i = 0; i < SDL_arraysize(active); i++) active[i] = false;
         for (int i = 0; i < SDL_arraysize(gate_active); i++) gate_active[i] = false;
@@ -155,7 +160,7 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     bg_texture = SDL_CreateTextureFromSurface(renderer, bg_surface); /* creates texture from sdl surface */
     SDL_RenderTexture(renderer, bg_texture, NULL, NULL);
 
-    for (int i = 0; i < SDL_arraysize(borders); i++)
+    for (int i = 0; i < SDL_arraysize(borders); i++)                   /* if gate is active, color border green, else red */
     {
         if (active[i])
             SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
@@ -164,14 +169,14 @@ SDL_AppResult SDL_AppIterate(void *appstate)
         SDL_RenderFillRect(renderer, &borders[i]);
     }
 
-    if (gate_active[2]) SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    if (gate_active[2]) SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);   /* result block border coloring */
     else SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderFillRect(renderer, &result_border);
 
     SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
-    SDL_RenderFillRects(renderer, wires, SDL_arraysize(wires));
+    SDL_RenderFillRects(renderer, wires, SDL_arraysize(wires));             /* wires connecting to blocks, mozda obojit i ovo jbg */
     
-    for (int i = 0; i < SDL_arraysize(gate_active); i++)
+    for (int i = 0; i < SDL_arraysize(gate_active); i++)                    /* logic operations loop */
     {
         switch (gate_op[i][0])
         {
@@ -179,7 +184,7 @@ SDL_AppResult SDL_AppIterate(void *appstate)
             if ((i == 0 && active[0] && active[1]) || (i == 1 && active[2] && active[3]) || (i == 2 && gate_active[0] && gate_active[1]))
             {
                 gate_active[i] = true;
-            } else gate_active[i] = false;
+            } else gate_active[i] = false;                                  /* checking if gates are true or false */
             break;
         case 'n':
             if ((i == 0 && !(active[0] && active[1])) || ((i == 1 && !(active[2] && active[3]))) || ((i == 2 && !(gate_active[0] && gate_active[1]))))
@@ -212,13 +217,13 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     
     for (int i = 0; i < SDL_arraysize(gate_active); i++)
     {
-        if (gate_active[i]) SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        if (gate_active[i]) SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);       /* coloring gate borders, active or not */
         else SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &gate_borders[i]);
     }
     
     SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
-    SDL_RenderFillRects(renderer, rects, SDL_arraysize(rects));
+    SDL_RenderFillRects(renderer, rects, SDL_arraysize(rects));                 /* rest of geometry, wires and rects */
 
     SDL_RenderFillRect(renderer, &result);
     SDL_RenderFillRects(renderer, gates, SDL_arraysize(gates));
@@ -227,15 +232,15 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     SDL_RenderFillRect(renderer, &right_wire);
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_SetRenderScale(renderer, 2.0f, 2.0f);
+    SDL_SetRenderScale(renderer, 2.0f, 2.0f);                       /* scaling because letters are too small */
     for (int i = 0; i < SDL_arraysize(gate_op); i++)
     {
         float x, y;
-        x = (gates[i].x + gates[i].w / 2 - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(gate_op[i])) / 2;
+        x = (gates[i].x + gates[i].w / 2 - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(gate_op[i])) / 2;    
         y = (gates[i].y + gates[i].h / 2 - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
         SDL_RenderDebugText(renderer, x, y, gate_op[i]);
     }
-    SDL_SetRenderScale(renderer, 1.0f, 1.0f);
+    SDL_SetRenderScale(renderer, 1.0f, 1.0f);                       /* reseting the scale */
 
     SDL_RenderPresent(renderer); /* presents renderer to window */
 
